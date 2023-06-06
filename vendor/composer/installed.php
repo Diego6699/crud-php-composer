3d6d20c5e952151a116e04aadebe421ee3b314da
Diego Leonardo da Silva França <diegoleonardo9966@gmail.com>
2023-06-06 10:14:52 -0300

    instalação do composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wdev/vagas',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '66d787a043a72f2a71f6684e9a1f93d178820ebc',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'wdev/vagas' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '66d787a043a72f2a71f6684e9a1f93d178820ebc',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
