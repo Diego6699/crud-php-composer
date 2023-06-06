eea1a7e16f82b879d89959b502e9284cd44bc171
Diego Leonardo da Silva França <diegoleonardo9966@gmail.com>
2023-06-06 10:42:23 -0300

    jumbotron do index

--- includes/header.php
+++ includes/header.php
@@ -1,9 +1,16 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>WDEV VAGAS!</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-  </head>
-  <body class="bg-dark text-light">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>WDEV VAGAS!</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+</head>
+
+<body class="bg-dark text-light">
+  <div class="container">
+    <div class="mt-4 p-5 bg-danger">
+      <h1>WDEV Vagas</h1>
+      <p>Exemplo de CRUD com PHP orientado a objetos</p>
+    </div>
