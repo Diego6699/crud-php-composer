459886891926b608e00d6c41d1875ae42884861c
Diego Leonardo da Silva França <diegoleonardo9966@gmail.com>
2023-06-08 10:56:56 -0300

    validaçao do POST

--- cadastrar.php
+++ cadastrar.php
@@ -1,8 +1,14 @@
 <?php
 require __DIR__."/vendor/autoload.php";
+
+use \App\Entity\Vaga;
+
 //validacao do post
 if(isset($_POST['titulo'] ,$_POST['descricao'],$_POST['ativo'])){
-    die("cadastrar");
+    $obVaga = new Vaga;
+    $obVaga->titulo = $_POST['titulo'];
+    $obVaga->descricao = $_POST['descricao'];
+    $obVaga->ativo = $_POST['ativo'];
 }
 include __DIR__."/includes/header.php";
 include __DIR__."/includes/formulario.php";
